eef85a195acd7f9d5ded2e7dd4183f1da16a9869
Renato Marques Teles <renato.teles@smn.domain>
2018-09-28 13:10:35 -0300

    adicionado as linhas, adicionado img da velha, melhorias no css

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var canvas, context;
+var contO = 0,contX = 0; // contadores do placar
+var linha = 0;  // verifica se a linha que indica o vencedor já foi desenhada
 var numero;     // variável que irá determinar se será desenhado bola ou X
 var tabuleiro = new Array(9).fill(null); // cria um vetor com as 9 posições do jogo, receberá 0 para bola e 1 para X
 window.onload = function () {   // essa função será executada no carregamento da página
@@ -62,7 +64,18 @@ function novoJogo(){
     document.getElementById("WinO").innerHTML = "";
     document.getElementById("WinX").innerHTML = "";
     document.getElementById("msgVencedor").innerHTML = "";
+    // Reseta as linhas
+    linha = 0;
+    // Reseta img de empate (Deu velha)
+    document.getElementById("imgVelha").innerHTML = "";
 }
+function zeraPlacar (){
+    contO = 0;
+    contX = 0;
+    document.getElementById("placar").innerHTML = "O:"+contO+" - X:"+contX;
+    novoJogo();
+}
+
 
 function posicaoMouse(canvas, event) {       
     var rect = canvas.getBoundingClientRect();  // função que vai pegar a posicao do mouse dentro do canvas
@@ -211,127 +224,228 @@ function naonulo(x) {
 }
 // Verificação se ganhou ou não
 function verifica() {
+    if(linha == 0){     // Verifica se já houve um vencedor
     // linhas
-    if((tabuleiro[0] == tabuleiro[1]) && (tabuleiro[1] == tabuleiro[2]) && (tabuleiro[0] != null)){
-        if(tabuleiro[1] == 0){
-            document.getElementById("WinO").innerHTML = "O";
+        if((tabuleiro[0] == tabuleiro[1]) && (tabuleiro[1] == tabuleiro[2]) && (tabuleiro[0] != null)){
+            if(tabuleiro[1] == 0){
+                document.getElementById("WinO").innerHTML = "O";
+                document.getElementById("WinX").innerHTML = "";
+                document.getElementById("msgVencedor").innerHTML = "VENCEDOR!";
+                contO++;
+                document.getElementById("placar").innerHTML = "O:"+contO+" - X:"+contX;
+                trava();
+            }
+            else{
+                document.getElementById("WinX").innerHTML = "X";
+                document.getElementById("WinO").innerHTML = "";
+                document.getElementById("msgVencedor").innerHTML = "VENCEDOR!";
+                contX++;
+                document.getElementById("placar").innerHTML = "O:"+contO+" - X:"+contX;
+                trava();
+            }
+            //Desenho da linha
+            context.beginPath();
+            context.moveTo(0, 100);
+            context.lineTo(600, 100);
+            context.lineWidth = 10;
+            context.strokeStyle = 'red';
+            context.stroke();
+            linha = 1;
+        }
+        else if((tabuleiro[3] == tabuleiro[4]) && (tabuleiro[4] == tabuleiro[5]) && (tabuleiro[3] != null)){
+            if(tabuleiro[3] == 0){
+                document.getElementById("WinO").innerHTML = "O";
+                document.getElementById("WinX").innerHTML = "";
+                document.getElementById("msgVencedor").innerHTML = "VENCEDOR!";
+                contO++;
+                document.getElementById("placar").innerHTML = "O:"+contO+" - X:"+contX;
+                trava();
+            }
+            else{
+                document.getElementById("WinX").innerHTML = "X";
+                document.getElementById("WinO").innerHTML = "";
+                document.getElementById("msgVencedor").innerHTML = "VENCEDOR!";
+                contX++;
+                document.getElementById("placar").innerHTML = "O:"+contO+" - X:"+contX;
+                trava();
+            }
+            //Desenho da linha
+            context.beginPath();
+            context.moveTo(0, 300);
+            context.lineTo(600, 300);
+            context.lineWidth = 10;
+            context.strokeStyle = 'red';
+            context.stroke();
+            linha = 1;
+        }
+        else if((tabuleiro[6] == tabuleiro[7]) && (tabuleiro[7] == tabuleiro[8]) && (tabuleiro[6] != null)){
+            if(tabuleiro[6] == 0){
+                document.getElementById("WinO").innerHTML = "O";
+                document.getElementById("WinX").innerHTML = "";
+                document.getElementById("msgVencedor").innerHTML = "VENCEDOR!";
+                contO++;
+                document.getElementById("placar").innerHTML = "O:"+contO+" - X:"+contX;
+                trava();
+            }
+            else{
+                document.getElementById("WinX").innerHTML = "X";
+                document.getElementById("WinO").innerHTML = "";
+                document.getElementById("msgVencedor").innerHTML = "VENCEDOR!";
+                contX++;
+                document.getElementById("placar").innerHTML = "O:"+contO+" - X:"+contX;
+                trava();
+            }
+            //Desenho da linha
+            context.beginPath();
+            context.moveTo(0, 500);
+            context.lineTo(600, 500);
+            context.lineWidth = 10;
+            context.strokeStyle = 'red';
+            context.stroke();
+            linha = 1;
+        }
+        // colunas
+        else if((tabuleiro[0] == tabuleiro[3]) && (tabuleiro[3] == tabuleiro[6]) && (tabuleiro[0] != null)){
+            if(tabuleiro[0] == 0){
+                document.getElementById("WinO").innerHTML = "O";
+                document.getElementById("WinX").innerHTML = "";
+                document.getElementById("msgVencedor").innerHTML = "VENCEDOR!";
+                contO++;
+                document.getElementById("placar").innerHTML = "O:"+contO+" - X:"+contX;
+                trava();
+            }
+            else{
+                document.getElementById("WinX").innerHTML = "X";
+                document.getElementById("WinO").innerHTML = "";
+                document.getElementById("msgVencedor").innerHTML = "VENCEDOR!";
+                contX++;
+                document.getElementById("placar").innerHTML = "O:"+contO+" - X:"+contX;
+                trava();
+            }
+            //Desenho da linha
+            context.beginPath();
+            context.moveTo(100, 0);
+            context.lineTo(100, 600);
+            context.lineWidth = 10;
+            context.strokeStyle = 'red';
+            context.stroke();
+            linha = 1;
+        }
+        else if((tabuleiro[1] == tabuleiro[4]) && (tabuleiro[4] == tabuleiro[7]) && (tabuleiro[1] != null)){
+            if(tabuleiro[1] == 0){
+                document.getElementById("WinO").innerHTML = "O";
+                document.getElementById("WinX").innerHTML = "";
+                document.getElementById("msgVencedor").innerHTML = "VENCEDOR!";
+                contO++;
+                document.getElementById("placar").innerHTML = "O:"+contO+" - X:"+contX;
+                trava();
+            }
+            else{
+                document.getElementById("WinX").innerHTML = "X";
+                document.getElementById("WinO").innerHTML = "";
+                document.getElementById("msgVencedor").innerHTML = "VENCEDOR!";
+                contX++;
+                document.getElementById("placar").innerHTML = "O:"+contO+" - X:"+contX;
+                trava();
+            }
+            //Desenho da linha
+            context.beginPath();
+            context.moveTo(300, 0);
+            context.lineTo(300, 600);
+            context.lineWidth = 10;
+            context.strokeStyle = 'red';
+            context.stroke();
+            linha = 1;
+        }
+        else if((tabuleiro[2] == tabuleiro[5]) && (tabuleiro[5] == tabuleiro[8]) && (tabuleiro[2] != null)){
+            if(tabuleiro[2] == 0){
+                document.getElementById("WinO").innerHTML = "O";
+                document.getElementById("WinX").innerHTML = "";
+                document.getElementById("msgVencedor").innerHTML = "VENCEDOR!";
+                contO++;
+                document.getElementById("placar").innerHTML = "O:"+contO+" - X:"+contX;
+                trava();
+            }
+            else{
+                document.getElementById("WinX").innerHTML = "X";
+                document.getElementById("WinO").innerHTML = "";
+                document.getElementById("msgVencedor").innerHTML = "VENCEDOR!";
+                contX++;
+                document.getElementById("placar").innerHTML = "O:"+contO+" - X:"+contX;
+                trava();
+            }
+            //Desenho da linha
+            context.beginPath();
+            context.moveTo(500, 0);
+            context.lineTo(500, 600);
+            context.lineWidth = 10;
+            context.strokeStyle = 'red';
+            context.stroke();
+            linha = 1;
+        }
+        // diagonais
+        else if((tabuleiro[0] == tabuleiro[4]) && (tabuleiro[4] == tabuleiro[8]) && (tabuleiro[0] != null)){
+            if(tabuleiro[0] == 0){
+                document.getElementById("WinO").innerHTML = "O";
+                document.getElementById("WinX").innerHTML = "";
+                document.getElementById("msgVencedor").innerHTML = "VENCEDOR!";
+                contO++;
+                document.getElementById("placar").innerHTML = "O:"+contO+" - X:"+contX;
+                trava();
+            }
+            else{
+                document.getElementById("WinX").innerHTML = "X";
+                document.getElementById("WinO").innerHTML = "";
+                document.getElementById("msgVencedor").innerHTML = "VENCEDOR!";
+                contX++;
+                document.getElementById("placar").innerHTML = "O:"+contO+" - X:"+contX;
+                trava();
+            }
+            //Desenho da linha
+            context.beginPath();
+            context.moveTo(0, 0);
+            context.lineTo(600, 600);
+            context.lineWidth = 10;
+            context.strokeStyle = 'red';
+            context.stroke();
+            linha = 1;
+        }
+        else if((tabuleiro[2] == tabuleiro[4]) && (tabuleiro[4] == tabuleiro[6]) && (tabuleiro[2] != null)){
+            if(tabuleiro[2] == 0){
+                document.getElementById("WinO").innerHTML = "O";
+                document.getElementById("WinX").innerHTML = "";
+                document.getElementById("msgVencedor").innerHTML = "VENCEDOR!";
+                contO++;
+                document.getElementById("placar").innerHTML = "O:"+contO+" - X:"+contX;
+                trava();
+            }
+            else{
+                document.getElementById("WinX").innerHTML = "X";
+                document.getElementById("WinO").innerHTML = "";
+                document.getElementById("msgVencedor").innerHTML = "VENCEDOR!";
+                contX++;
+                document.getElementById("placar").innerHTML = "O:"+contO+" - X:"+contX;
+                trava();
+            }
+            //Desenho da linha
+            context.beginPath();
+            context.moveTo(600, 0);
+            context.lineTo(0, 600);
+            context.lineWidth = 10;
+            context.strokeStyle = 'red';
+            context.stroke();
+            linha = 1;
+        }
+        //Empate
+        else if (tabuleiro.every(x => x !== null)){
             document.getElementById("WinX").innerHTML = "";
-            document.getElementById("msgVencedor").innerHTML = "VENCEDOR!";
-            trava();
-        }
-        else{
-            document.getElementById("WinX").innerHTML = "X";
             document.getElementById("WinO").innerHTML = "";
-            document.getElementById("msgVencedor").innerHTML = "VENCEDOR!";
+            document.getElementById("msgVencedor").innerHTML = "Deu velha!";
+            document.getElementById("imgVelha").innerHTML = "<img src=\"css/img/granny.png\">";
+            document.getElementById("placar").innerHTML = "O:"+contO+" - X:"+contX;
             trava();
+            linha = 1;
         }
-    }
-    else if((tabuleiro[3] == tabuleiro[4]) && (tabuleiro[4] == tabuleiro[5]) && (tabuleiro[3] != null)){
-        if(tabuleiro[3] == 0){
-            document.getElementById("WinO").innerHTML = "O";
-            document.getElementById("WinX").innerHTML = "";
-            document.getElementById("msgVencedor").innerHTML = "VENCEDOR!";
-            trava();
-        }
-        else{
-            document.getElementById("WinX").innerHTML = "X";
-            document.getElementById("WinO").innerHTML = "";
-            document.getElementById("msgVencedor").innerHTML = "VENCEDOR!";
-            trava();
-        }
-    }
-    else if((tabuleiro[6] == tabuleiro[7]) && (tabuleiro[7] == tabuleiro[8]) && (tabuleiro[6] != null)){
-        if(tabuleiro[6] == 0){
-            document.getElementById("WinO").innerHTML = "O";
-            document.getElementById("WinX").innerHTML = "";
-            document.getElementById("msgVencedor").innerHTML = "VENCEDOR!";
-            trava();
-        }
-        else{
-            document.getElementById("WinX").innerHTML = "X";
-            document.getElementById("WinO").innerHTML = "";
-            document.getElementById("msgVencedor").innerHTML = "VENCEDOR!";
-            trava();
-        }
-    }
-    // colunas
-    else if((tabuleiro[0] == tabuleiro[3]) && (tabuleiro[3] == tabuleiro[6]) && (tabuleiro[0] != null)){
-        if(tabuleiro[0] == 0){
-            document.getElementById("WinO").innerHTML = "O";
-            document.getElementById("WinX").innerHTML = "";
-            document.getElementById("msgVencedor").innerHTML = "VENCEDOR!";
-            trava();
-        }
-        else{
-            document.getElementById("WinX").innerHTML = "X";
-            document.getElementById("WinO").innerHTML = "";
-            document.getElementById("msgVencedor").innerHTML = "VENCEDOR!";
-            trava();
-        }
-    }
-    else if((tabuleiro[1] == tabuleiro[4]) && (tabuleiro[4] == tabuleiro[7]) && (tabuleiro[1] != null)){
-        if(tabuleiro[1] == 0){
-            document.getElementById("WinO").innerHTML = "O";
-            document.getElementById("WinX").innerHTML = "";
-            document.getElementById("msgVencedor").innerHTML = "VENCEDOR!";
-            trava();
-        }
-        else{
-            document.getElementById("WinX").innerHTML = "X";
-            document.getElementById("WinO").innerHTML = "";
-            document.getElementById("msgVencedor").innerHTML = "VENCEDOR!";
-            trava();
-        }
-    }
-    else if((tabuleiro[2] == tabuleiro[5]) && (tabuleiro[5] == tabuleiro[8]) && (tabuleiro[2] != null)){
-        if(tabuleiro[2] == 0){
-            document.getElementById("WinO").innerHTML = "O";
-            document.getElementById("WinX").innerHTML = "";
-            document.getElementById("msgVencedor").innerHTML = "VENCEDOR!";
-            trava();
-        }
-        else{
-            document.getElementById("WinX").innerHTML = "X";
-            document.getElementById("WinO").innerHTML = "";
-            document.getElementById("msgVencedor").innerHTML = "VENCEDOR!";
-            trava();
-        }
-    }
-    // diagonais
-    else if((tabuleiro[0] == tabuleiro[4]) && (tabuleiro[4] == tabuleiro[8]) && (tabuleiro[0] != null)){
-        if(tabuleiro[0] == 0){
-            document.getElementById("WinO").innerHTML = "O";
-            document.getElementById("WinX").innerHTML = "";
-            document.getElementById("msgVencedor").innerHTML = "VENCEDOR!";
-            trava();
-        }
-        else{
-            document.getElementById("WinX").innerHTML = "X";
-            document.getElementById("WinO").innerHTML = "";
-            document.getElementById("msgVencedor").innerHTML = "VENCEDOR!";
-            trava();
-        }
-    }
-    else if((tabuleiro[2] == tabuleiro[4]) && (tabuleiro[4] == tabuleiro[6]) && (tabuleiro[2] != null)){
-        if(tabuleiro[2] == 0){
-            document.getElementById("WinO").innerHTML = "O";
-            document.getElementById("WinX").innerHTML = "";
-            document.getElementById("msgVencedor").innerHTML = "VENCEDOR!";
-            trava();
-        }
-        else{
-            document.getElementById("WinX").innerHTML = "X";
-            document.getElementById("WinO").innerHTML = "";
-            document.getElementById("msgVencedor").innerHTML = "VENCEDOR!";
-            trava();
-        }
-    }
-    //Empate
-    else if (tabuleiro.every(x => x !== null)){
-        document.getElementById("WinX").innerHTML = "";
-        document.getElementById("WinO").innerHTML = "";
-        document.getElementById("msgVencedor").innerHTML = "Deu velha!";
-        trava();
     }
 }
 
@@ -341,4 +455,13 @@ function trava() {      // impede que o usuário continue jogando após anunciad
             tabuleiro[i] = 3;       // preenche as posições vazias do tabuleiro
         }
     }
+}
+
+function risco() {
+    context.beginPath();
+    context.moveTo(0, 0);
+    context.lineTo(600, 200);
+    context.lineWidth = 2;
+    context.strokeStyle = 'red';
+    context.stroke();
 }
